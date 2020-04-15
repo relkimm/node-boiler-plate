9b4a9e5d0e138ec09adfd856be4df4847486fa8d
chillgu <bbonggu92@gmail.com>
2020-04-15 22:12:35 +0900

    added register route

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const port = 3000;
 
 const mongoose = require("mongoose");
 
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
+
 mongoose
   .connect(
     "mongodb+srv://chillgu:abcd1234@boiler-plate-12xps.mongodb.net/test?retryWrites=true&w=majority",
@@ -14,10 +23,22 @@ mongoose
       useFindAndModify: false,
     }
   )
-  .then(() => console.log("mongoDB is connected"))
+  .then(() => console.log("mongoDB is connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) =>
   res.send("Hello chillgu~ Welcome to node express world!")
 );
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  // userModel에 저장됨.
+  // doc -> user에 대한 정보가 담겨있다.
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
 app.listen(port, () => console.log(`I'm waiting for port 3000~~`));
